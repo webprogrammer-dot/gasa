3af68b67b01d157035500629e026b38866adc1f6
webprogrammer-dot <63945817+webprogrammer-dot@users.noreply.github.com>
2020-05-01 04:26:19 -0700

    Add files via upload

---
+++ sayuj c programes/16_hw2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style>
+li { padding: 5px; }
+</style>
+<link href="../../static/prism.css" rel="stylesheet"/>
+</head>
+<body style="margin-left:5%; margin-bottom: 60px; margin-right: 5%; font-family: sans-serif;">
+  <div class = "visible-print-block">
+  <b style = "float:right">www.wd4e.com</b>
+  </div>
+  <h1>Homework Two: Advanced Selectors</h1>
+  <p>This homework is also described in a video lecture. Make sure to watch it to get a better feel for what I am looking for.</p>
+  <h3>Objectives</h3>
+  <ol>
+    <li>Create a style sheet and link it to the html files. (Without changing the html.)</li>
+    <li>Style the sheet using the properties we covered in lecture.</li>
+    <li>Consider using some elements we didn’t cover, or didn’t cover well. You can search for the options online, or you can watch the videos from Week Four for some pointers.</li>
+  </ol>
+  <br>
+  <h3>Background</h3>
+  <p>I assume that you have finished all of the Week Two lecture videos, and you may want to watch Week Three as you complete this assignment. This assignment builds on Assignment One, but we have a new set of “After” pictures. You can see the newly styled pages here:</p>
+  <p><a href="http://intro-webdesign.com/CSS/assignment-2/index.jpg">http://intro-webdesign.com/CSS/assignment-2/index.jpg</a></p>
+  <p><a href="http://www.intro-webdesign.com/CSS/assignment-2/teams.jpg">http://www.intro-webdesign.com/CSS/assignment-2/teams.jpg</a></p>
+  <p><a href="http://www.intro-webdesign.com/CSS/assignment-2/history.jpg">http://www.intro-webdesign.com/CSS/assignment-2/history.jpg</a></p>
+  <br>
+  <h3>Getting Started</h3>
+  <p>You will use the same code as you used for Assignment One. I would recommend that you save a copy of your stylesheet for backup. The html is all still the same.</p>
+  <h3>Steps</h3>
+  <ol>Download the image from <a href="http://intro-webdesign.com/CSS/assignment-2/images/flywheel.jpg">http://intro-webdesign.com/CSS/assignment-2/images/flywheel.jpg</a></ol>
+  <ol>Style the following</ol>
+    <li>The body should have padding and margin</li>
+    <li>The header should have a background color, background image (using a local image), and padding</li>
+    <li>The navigation links (and only the links in the navigation) should be styled with a new display, margin, border-radius, text-decoration, and padding</li>
+    <li>The link to the current page should be styled differently from the other links (using class = "active')</li>
+    <li>The sections should be next to each other, not one on top of the other.</li>
+  </ol>
+  <p><b><i> have included a video you can watch to see a live demo of this new site. You can watch it in the next video, or link directly here </i></b><a href="https://www.coursera.org/learn/introcss/lecture/VQgU9/02-08-optional-homework-description">https://www.coursera.org/learn/introcss/lecture/VQgU9/02-08-optional-homework-description</a></p>
+  <h3>Submission</h3>
+  <ol>Submit your css file, we will still call it hw1.css.</ol>
+  <ol>Use the peer grader to assess the work of your fellow students. This second assignment may have a number of different options. You can grade the assignment by eye, or else save the css and try applying it to the html files.</ol>
+  <br>
+  <h3>Before</h3>
+  <p><a href "../../images/css3/hw2_before.png"></a></p>
+  <p><a href "../../images/css3/hw2_after.png"></a></p>
+</body>
+</html>
